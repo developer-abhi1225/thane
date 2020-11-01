efa23fa9aba952879c1dfa3496b2d46fb4ba49db
abhishek <abhi.l1225@gmail.com>
2020-11-01 16:24:43 +0530

    added env to FE

--- FrontEnd/src/Constants/apiUrl.js
+++ FrontEnd/src/Constants/apiUrl.js
@@ -1,1 +1,3 @@
-export const APIURL = "http://localhost:4000/"
+console.log("REACT ENVS ", process.env.REACT_APP_BE_HOST);
+console.log("REACT ENVS ", process.env.REACT_APP_BE_PORT);
+export const APIURL = `http://${process.env.REACT_APP_BE_HOST}:${process.env.REACT_APP_BE_PORT}/`;
